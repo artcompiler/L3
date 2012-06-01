b8e7a05fab457be8d1b2afea2be2171860cea49f
Jeff Dyer <jeffdyer@acm.org>
2012-06-01 16:42:47 -0700

    link to defs

--- test/t.js
+++ test/t.js
@@ -1,40 +1,8 @@
-/*
-(function ($, _) {
-    $.x();
-    $.y();
-    $.z();
-    var v = Backbone.View.extend({ $: $,
-				   _: _
-				 })
-})({ x: function () { print('x') },
-     y: function () { print('y') },
-     z: function () { print('z') }
-   },
-   { a: { foo: "foo",
-	  bar: "bar",
-	  baz: "baz"
-	}
-   }
-)
-*/
-/*
-  // Returns a function that will be executed at most one time, no matter how
-  // often you call it. Useful for lazy initialization.
-  _.once = function(func) {
-    var ran = false, memo;
-    return function() {
-      if (ran) return memo;
-      ran = true;
-      return memo = func.apply(this, arguments);
-    };
-  };
-
-*/
-
-function f() {
-    return x + y
+function f(x, y) {
+    function g() {
+        return x + y
+    }
+    return g()
 }
 
-function g() {
-    return x + y + z
-}
+f(1,2)
